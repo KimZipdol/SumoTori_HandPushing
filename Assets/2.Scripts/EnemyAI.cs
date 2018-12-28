1f0c3384b94fca78e27fd751b0d0a90bbb29f77d
JimoonKim <wlansdl@gmail.com>
2018-12-28 14:45:15 +0900

    #3

---
+++ Assets/2.Scripts/EnemyAI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    public GameObject target;
+
+    WaitForSeconds ws;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
